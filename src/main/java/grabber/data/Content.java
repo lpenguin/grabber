48cea6fb5cd93fc9855a89ba6fd05371112a5ec9
lpenguin <lpenguin@yandex.ru>
2014-04-03 10:31:39 +0400

    db

---
+++ src/main/java/grabber/data/Content.java
@@ -0,0 +1,7 @@
+package grabber.data;
+
+/**
+ * Created by nikita on 30.03.14.
+ */
+public class Content {
+}
---
+++ src/main/java/grabber/data/database/Connection.java
@@ -0,0 +1,7 @@
+package grabber.data.database;
+
+/**
+ * Created by nikita on 30.03.14.
+ */
+public class Connection {
+}
